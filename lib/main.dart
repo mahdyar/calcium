7ddbaae57ae35a253242e98c0fabc9a7e861b41a
Mahdyar Hasanpour <hi@mahdyar.me>
2021-06-01 14:03:49 +0430

    chore: add developer comments

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:calcium/pages/MainPage.dart';
 import 'package:calcium/constants.dart';
 
+/*
+ * 
+ * 
+ * Developed by: mahdyar.me
+ * Contact: hi@mahdyar.me
+ * Github: https://git.io/mahdyar-calcium
+ * 
+ * 
+*/
+
 void main() {
   runApp(MainApp());
 }
@@ -12,7 +22,9 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       title: 'Calcium',
       theme: ThemeData(
-          primaryColor: kMainColor, scaffoldBackgroundColor: kMainColor, primarySwatch: Colors.teal),
+          primaryColor: kMainColor,
+          scaffoldBackgroundColor: kMainColor,
+          primarySwatch: Colors.teal),
       home: MainPage(title: 'Calcium'),
     );
   }
