07c3c18d10a5439b4610f11809dd913436dc0524
Mahdyar Hasanpour <hi@mahdyar.me>
2021-06-01 12:21:42 +0430

    feat: create mainpage ui

--- lib/pages/MainPage.dart
+++ lib/pages/MainPage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:calcium/widgets/CalculatorButton.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 class MainPage extends StatefulWidget {
   MainPage({Key? key, required this.title}) : super(key: key);
   final String title;
@@ -13,12 +15,128 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // appBar: AppBar(
-        //   centerTitle: true,
-        //   title: Text(widget.title),
-        // ),
-        backgroundColor: Color(0xFF283637),
-        body: Center(child: CalculatorButton(label: 'AC',)),
+      // appBar: AppBar(
+      //   centerTitle: true,
+      //   title: Text(widget.title),
+      // ),
+      backgroundColor: Color(0xFF283637),
+      body: Column(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+        Container(
+          padding: EdgeInsets.only(right: 12),
+          child: Text(
+            '123*123',
+            style: GoogleFonts.rubik(
+                textStyle: TextStyle(fontSize: 24), color: Color(0xFF545F61)),
+          ),
+          alignment: Alignment(1, 1),
+        ),
+        Container(
+          padding: EdgeInsets.all(12),
+          child: Text(
+            '123',
+            style: GoogleFonts.rubik(
+                textStyle: TextStyle(fontSize: 48), color: Colors.white),
+          ),
+          alignment: Alignment(1, 1),
+        ),
+        SizedBox(height: 20),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              CalculatorButton(
+                label: 'AC',
+                fillColor: 0xFF6C807F,
+                textSize: 24,
+              ),
+              CalculatorButton(
+                label: 'C',
+                fillColor: 0xFF6C807F,
+              ),
+              CalculatorButton(
+                label: '%',
+                fillColor: 0xFFFFFFFF,
+                textColor: 0xFF65BDAC,
+              ),
+              CalculatorButton(
+                label: '/',
+                fillColor: 0xFFFFFFFF,
+                textColor: 0xFF65BDAC,
+              ),
+            ]),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              CalculatorButton(
+                label: '7',
+              ),
+              CalculatorButton(
+                label: '8',
+              ),
+              CalculatorButton(
+                label: '9',
+              ),
+              CalculatorButton(
+                label: '×',
+                fillColor: 0xFFFFFFFF,
+                textColor: 0xFF65BDAC,
+              ),
+            ]),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              CalculatorButton(
+                label: '4',
+              ),
+              CalculatorButton(
+                label: '5',
+              ),
+              CalculatorButton(
+                label: '6',
+              ),
+              CalculatorButton(
+                label: '-',
+                fillColor: 0xFFFFFFFF,
+                textColor: 0xFF65BDAC,
+              ),
+            ]),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              CalculatorButton(
+                label: '1',
+              ),
+              CalculatorButton(
+                label: '2',
+              ),
+              CalculatorButton(
+                label: '3',
+              ),
+              CalculatorButton(
+                label: '+',
+                fillColor: 0xFFFFFFFF,
+                textColor: 0xFF65BDAC,
+              ),
+            ]),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              CalculatorButton(
+                label: '.',
+              ),
+              CalculatorButton(
+                label: '0',
+              ),
+              CalculatorButton(
+                label: '00',
+                textSize: 26,
+              ),
+              CalculatorButton(
+                label: '=',
+                fillColor: 0xFFFFFFFF,
+                textColor: 0xFF65BDAC,
+              ),
+            ]),
+      ]),
     );
   }
 }
--- lib/widgets/CalculatorButton.dart
+++ lib/widgets/CalculatorButton.dart
@@ -6,32 +6,35 @@ class CalculatorButton extends StatelessWidget {
       {required this.label,
       this.textColor = 0xFFFFFFFF,
       this.fillColor = 0x00ffffff,
-      this.textSize = 24});
+      this.textSize = 28});
   final String label;
   final int textColor;
   final int fillColor;
   final double textSize;
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-        width: 65,
-        height: 65,
-        child: TextButton(
-          onPressed: () {},
-          child: Text(label,
-              style:
-                  GoogleFonts.rubik(textStyle: TextStyle(fontSize: textSize))),
-          style: ButtonStyle(
-              padding: MaterialStateProperty.all<EdgeInsets>(
-                  EdgeInsets.fromLTRB(15, 5, 15, 5)),
-              backgroundColor:
-                  MaterialStateProperty.all<Color>(Color(fillColor)),
-              foregroundColor:
-                  MaterialStateProperty.all<Color>(Color(textColor)),
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(50),
-              ))),
-        ));
+    return Container(
+      margin: EdgeInsets.all(10),
+      child: SizedBox(
+          width: 65,
+          height: 65,
+          child: TextButton(
+            onPressed: () {},
+            child: Text(label,
+                style:
+                    GoogleFonts.rubik(textStyle: TextStyle(fontSize: textSize))),
+            style: ButtonStyle(
+                padding: MaterialStateProperty.all<EdgeInsets>(
+                    EdgeInsets.fromLTRB(15, 5, 15, 5)),
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Color(fillColor)),
+                foregroundColor:
+                    MaterialStateProperty.all<Color>(Color(textColor)),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50),
+                ))),
+          )),
+    );
   }
 }
